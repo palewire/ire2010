48d2005119b9f76d50906c629fb95c085b498c1b
palewire <ben.welsh@gmail.com>
2010-06-10 06:34:51 -0700

    Got the detail page HTML and SWF from Aron and got it wired up.

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,38 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render_to_response
+# Models
 from polls.models import Project, Vote
-from django.http import HttpResponseRedirect, HttpResponse
+
+# Response helpers
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render_to_response
+
+# Aggregation
 from django.db.models import Sum
 
+
 def index(request):
+    """
+    The homepage. A list of all Projects.
+    """
     projects = Project.objects.all().order_by('-pub_date')[:5]
     return render_to_response('polls/index.html', {'projects': projects})
 
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    """
+    A detail page about one particular Project.
+    """
+    p = Project.objects.get(pk=poll_id)
+    total = p.vote_set.count()
+    return render_to_response('polls/detail.html', {'project': p, 'vote_total': total, })
 
 
 def vote(request, poll_id):
+    """
+    Instructs the databse to vote a projects up or down.
+
+    Manipulated by our Flashy friend.
+    """
     p = get_object_or_404(Project, pk=poll_id)
     if request.POST['data'] == "0":
         value = -1
@@ -26,9 +44,23 @@ def vote(request, poll_id):
 
 
 def data(request, poll_id):
+    """
+    Serves the XML data that fuels the Flash toy.
+    """
     p = Project.objects.get(pk=poll_id)
     total = p.vote_set.aggregate(Sum('choice'))
-    return render_to_response('polls/data.xml', {'project': p, 'vote_total': total['choice__sum'], }, mimetype="text/xml")
+    return render_to_response('polls/data.xml', {
+        'project': p,
+        'vote_total': total['choice__sum'],
+        }, mimetype="text/xml")
+
 
 def crossdomain(request):
-    return HttpResponse('<?xml version=\"1.0\"?><cross-domain-policy><allow-access-from domain=\"*\" /></cross-domain-policy>', mimetype="text/xml")
+    """
+    A bit of Flash bookkeeping.
+    """
+    response = '<?xml version=\"1.0\"?><cross-domain-policy><allow-access-from domain=\"*\" /></cross-domain-policy>'
+    return HttpResponse(response, mimetype="text/xml")
+
+
+
--- urls.py
+++ urls.py
@@ -7,21 +7,19 @@ admin.autodiscover()
 from django.conf import settings
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^project/', include('project.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    # The Django admin
     (r'^admin/', include(admin.site.urls)),
 
+    # The `polls` app
     (r'^polls/$', 'polls.views.index'),
     (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
     (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
     (r'^polls/(?P<poll_id>\d+)/data.xml$', 'polls.views.data'),
+
+    # Static media
     (r'^crossdomain.xml$', 'polls.views.crossdomain'),
-    (r'^pages/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT }),
+    (r'^local-media/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': settings.STATIC_DOC_ROOT }),
 
 )
